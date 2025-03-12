85119c2f09809aea74c120298cc973e4ce51af1d
Karan Yede <115563014+karanyede@users.noreply.github.com>
2025-03-13 01:08:09 +0530

    Update selectionnSort.java

--- Sorting/selectionnSort.java
+++ Sorting/selectionnSort.java
@@ -1,5 +1,17 @@
-class solution{
-public static void main(String args[]){
-  System.out.println();
-}
+class Solution {
+    void selectionSort(int[] arr) {
+      //First we need to compare whole array with index 0 and swap with smallest element
+      //then move to next index and do the same with next indexes
+        for(int i=0;i<arr.length-1;i++){
+            int min =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp = arr[i];
+            arr[i]= arr[min];
+            arr[min]=temp;
+        }
+    }
 }
